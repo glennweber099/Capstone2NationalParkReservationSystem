ba54b0ff0216dff11193d48ed91dcf45997fba6a
Brandon Best <bestb23@yahoo.com>
2019-10-23 15:54:14 -0400

    wrote classes with properties

---
+++ 09_Capstone/Capstone/Models/Campground.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Campground
+    {
+        public int Id { get; set; }
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public int OpenMonth { get; set; }
+        public int CloseMonth { get; set; }
+        public decimal DailyFee { get; set; }
+
+    }
+}
